4f7a836c94040c8d8738129e538f73528f0ce2fb
varunlad <varunlad5@gmail.com>
2021-08-30 00:23:24 +0530

    new practice

---
+++ sample1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+            Console.Write("This is my sample project " );
+            for(i=1; i<=20; i++)
+            {
+                i =  i * 2;
+                Console.Write(i + " ");
+                Console.Read();
+                
+            }
+        }
+    }
+}
